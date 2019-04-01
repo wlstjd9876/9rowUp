dd6eea09192c0804373ca5411fca456273cb2024
JSPark <wlstjd-0-@hanmail.net>
2019-04-02 02:17:29 +0900

    ajax 셀렉문 작성중(50% 완성), 검색폼 디자인 완료

--- FinalProject/src/main/webapp/resources/js/data/location/searchform.js
+++ FinalProject/src/main/webapp/resources/js/data/location/searchform.js
@@ -1,7 +1,7 @@
 //================== 서치 시작=====================//
 $.fn.toggleState = function(b) {
 	$(this).stop().animate({
-		width : b ? "300px" : "50px"
+		width : b ? "494px" : "50px"
 	}, 600, "easeOutElastic");
 };
 
@@ -51,51 +51,22 @@ $(document).ready(function() {
 //================== 서치 끝 ======================//
 //==================셀렉 시작======================//
 
-function doChange(srcE, targetId){
+function doChange(srcE){
 	var val = srcE.options[srcE.selectedIndex].value;
-	var targetE = document.getElementById(targetId);
 	$.ajax({
-		url : "/data/sigunguAjax.do",
+		url : "sigunguAjax.do",
 		type: "post",
 		dataType: "json",
 		data : {val :val},
-		success:function(data){	 
-
+		success:function(data){
+			$("#sigungucode").find("option").remove().end().append("<option value=''>시군구</option>");
+			$(data.list).each(function(index, result){
+				$("#sigungucode").append("<option value='"+(index+1)+"'>"+result+"</option>");
+			});
 		},
 		error:function(request,status,error){
 			alert("uniteCommonAjax:request:"+request+"status:"+status+"error:"+error);
 		}
 	});	
-	alert(val);
-	removeAll(targetE);
-	if(val == 'languages'){
-		addOption('C++', targetE);
-		addOption('Java', targetE);
-		addOption('Scheme', targetE);
-	}
-	else if(val == 'tools'){
-		addOption('Visual Studio', targetE);
-		addOption('Netbeans', targetE);
-		addOption('Eclipse', targetE);
-	}
 }
-
-function addOption(value, e){
-	var o = new Option(value);
-	try{
-		e.add(o);
-	}catch(ee){
-		e.add(o, null);
-	}
-}
-
-function removeAll(e){
-	for(var i = 0, limit = e.options.length; i < limit - 1; ++i){
-		e.remove(1);
-	}
-}
-
-
-
-
 //=================셀렉 끝======================//
